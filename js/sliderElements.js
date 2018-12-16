73a45f29f1db1fae0a4c1ef5f8f0090300abd9a8
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-12-16 19:16:31 +0300

    Add files via upload

--- js/sliderElements.js
+++ js/sliderElements.js
@@ -1,4 +1,20 @@
-var prev = document.querySelectorAll('.bx-wrapper')[0];
-console.log(prev);
 
-prev.innerHTML ="ree";
+$(document).ready(function() {
+  $('.slider').bxSlider({
+    auto: true,
+    pager: false,
+    nextSelector: $('.next0'),
+    prevSelector: $('.prev0')
+  });
+
+  $('.recomend-slide').slick({
+    slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2800,
+   prevArrow:$('.prev1'),
+   nextArrow:$('.next1')
+  });
+
+
+});
